b326d8b6e9ab9f15ee82d5c73584572c91f60add
Claudio Acioli <kllaudyo@gmail.com>
2018-04-17 14:08:46 -0300

    feat: Resolve exercicios tente isso

---
+++ src/com/acioli/claudio/ch1/GalToLit.java
@@ -0,0 +1,12 @@
+package com.acioli.claudio.ch1;
+
+public class GalToLit {
+
+    public static void main(String[] args){
+        double gallons, liters; //declara galoes e litros
+        gallons = 10;
+        liters = gallons * 3.7854;
+        System.out.println(gallons + " gallons is " + liters + " liters.");
+    }
+
+}
---
+++ src/com/acioli/claudio/ch1/GalToLitTable.java
@@ -0,0 +1,21 @@
+package com.acioli.claudio.ch1;
+
+public class GalToLitTable {
+
+    public static void main(String[] args){
+        double gallons, liters;
+        int counter;
+
+        counter = 0;
+        for(gallons = 1; gallons <= 100; gallons++){
+            liters = gallons * 3.7854;
+            System.out.println(gallons + " gallons is " + liters + " liters.");
+            counter++;
+            if(counter == 10){
+                System.out.println();
+                counter = 0;
+            }
+        }
+    }
+
+}
