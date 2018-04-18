f79c47fdfe5a3a7e57e6f47b91345e216e702d5b
Claudio Acioli <kllaudyo@gmail.com>
2018-04-18 20:12:57 -0300

    feat: Resolve desafio peso na lua

---
+++ src/com/acioli/claudio/ch1/EarthToMoon.java
@@ -0,0 +1,11 @@
+package com.acioli.claudio.ch1;
+
+public class EarthToMoon {
+
+    public static void main(String[] args){
+        double earth = 78.0, moon;
+        moon = earth * 0.17;
+        System.out.println("Your have a "+ earth +" in earth, but in the moon is " + moon);
+    }
+
+}
