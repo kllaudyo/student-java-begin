0933e0342aa75fb2a7ed87714f0b872b4b1daeaf
Claudio Acioli <kllaudyo@gmail.com>
2018-04-19 21:24:59 -0300

    feat: Adiciona novas opções de ajuda

--- src/com/acioli/claudio/ch3/Help.java
+++ src/com/acioli/claudio/ch3/Help.java
@@ -7,14 +7,22 @@ public class Help{
     public static void main(String args[])
             throws IOException {
 
-        System.out.println("Help on:");
-        System.out.println("\t1. if");
-        System.out.println("\t2. switch");
-        System.out.print("Choose one: ");
+        char choice, ignore;
 
-        char choice = (char) System.in.read();
+        do {
 
-        System.out.println("\n");
+            System.out.println("Help on:");
+            System.out.println("\t1. if");
+            System.out.println("\t2. switch");
+            System.out.println("\t3. for");
+            System.out.println("\t4. while");
+            System.out.println("\t5. do-while\n");
+            System.out.print("Choose one: ");
+
+            choice = (char) System.in.read();
+            do ignore = (char) System.in.read(); while (ignore != '\n');
+
+        }while(choice < '1' | choice > '5');
 
         switch (choice){
             case '1':
@@ -30,6 +38,22 @@ public class Help{
                 System.out.println("\t\t break;");
                 System.out.println("\t...");
                 System.out.println("}");
+                break;
+            case '3':
+                System.out.println("The for:\n");
+                System.out.println("for(inicializacao; condição; iteração)");
+                System.out.println("\tinstrução;");
+                break;
+            case '4':
+                System.out.println("The while:\n");
+                System.out.println("while(condição)");
+                System.out.println("\tinstrução;");
+                break;
+            case '5':
+                System.out.println("The do-while:\n");
+                System.out.println("do {");
+                System.out.println("\tinstrução;");
+                System.out.println("} while (condição);");
                 break;
             default:
                 System.out.println("Selection not found");
