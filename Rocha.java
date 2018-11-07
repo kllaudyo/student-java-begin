dc158beee9f63c4231d51ce8f6ce12d0e0158205
Claudio Acioli <kllaudyo@gmail.com>
2018-11-07 09:23:02 -0200

    Update Rocha.java

--- Rocha.java
+++ Rocha.java
@@ -1,53 +1,37 @@
-//Importa classe necessária para gerar número randomico
 import java.util.Random;
 
-//Toda classe publica deve ter o mesmo nome do arquivo ".java", essa classe obrigatoriamente deve se chamar Rocha.java
 public class Rocha{
 
-	//
 	private static int tentativas = 0;
 
-	//Método chamado pelo comando java Rocha apos compilação
-	//Método main é o método principal de qualquer classe java sendo o mesmo o ponto de partida da rotina
 	public static void main(String []args){
 
-		//Instancia classe que gerará o número randômico 
-		Random r = new Random();
-		
-		//Método que retorna um interio de 0 a 100( o parametro define o número máximo )
+		Random r = new Random();		
 		int numero = r.nextInt(100);
-
-		//variavel que irá guardar a resposta do usuário
 		int resposta;
+		do resposta = registraTentativa(numero); while(numero != resposta);
 
-		//Escolha do loop pois o programa ira funcionar até o usuário acertar a resposta
-		//A escolha do loop "do" é porque ele sempre irá realizar a rotina ao menos uma vez
-		do{
+	}
 
-			if(tentativas == 0){
-				System.out.println("Tente advinhar o numero que estou pensando de 1 a 100");	
-			}else{
-				System.out.println("Tente novamente, boa sorte:");	
-			}
+	private static int registraTentativa(int numero){
 
-			//Lê a resposta do usuário via linha de comando
-			resposta = Integer.parseInt(System.console().readLine());
+		if(tentativas == 0)
+			System.out.println("Tente advinhar o numero que estou pensando de 1 a 100");	
+		else
+			System.out.println("Tente novamente, boa sorte:");	
 
-			//ATENÇÃO aqui esta o pulo do gato do exercício, a variavel de classe tentativas
-			//só pode ser atribuída desta forma por conta de ser uma variável estatíca
-			//aqui poderia ser armazenado em uma variavel local, porém a orientação a objeto não seria utilizada
-			//IMPORTANTÍSSIMO entender o conceito de "variáveis de classe"
-			tentativas++;				
-			
-			if(numero == resposta){
-				System.out.println("Parabéns você acertou em " + tentativas + " tentativas");
-			}else if(numero > resposta){
-				System.out.println("Opa você errou! Dica: É mais do que você pensou");
-			}else if(numero < resposta){
-				System.out.println("Opa você errou! Dica: É menos do que você pensou");
-			}			
-			
-		//Finaliza loop quando usuário acertar a resposta			
-		}while(numero != resposta);
+		int resposta = Integer.parseInt(System.console().readLine());
+
+		tentativas++;				
+		
+		if(numero == resposta)
+			System.out.println("Parabens voce acertou em " + tentativas + " tentativas");
+		else if(numero > resposta)
+			System.out.println("Opa voce errou! Dica: E mais do que voce pensou");
+		else if(numero < resposta)
+			System.out.println("Opa voce errou! Dica: E menos do que voce pensou");
+
+		return resposta;
+
 	}
 }
